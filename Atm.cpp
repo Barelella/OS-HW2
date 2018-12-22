f3a2d35016756b2843d530df5d6e674f6931e908
shemeshn <shemeshn@campus.technion.ac.il>
2018-12-22 02:32:22 +0200

    Changed solution structure, parsed terminal command and input files commands created ATM threads

--- Atm.cpp
+++ Atm.cpp
@@ -7,15 +7,71 @@
 
 #include "Atm.h"
 
-Atm::Atm() {
-	// TODO Auto-generated constructor stub
+const int MAX_ARG = 15;
+
+Atm::Atm(Bank& bank, int serialNumber, string fileName, ofstream& log) :
+	bank(bank), serialNumber(serialNumber), fileName(fileName), log(log){
 
 }
 
 Atm::~Atm() {
-	// TODO Auto-generated destructor stub
+
 }
 
 void Atm::Run(){
-	return;
+	cout << "Atm no. " << serialNumber << " runs with input file " << fileName << endl;
+	std::ifstream file(fileName.c_str());
+	string currentCommand;
+
+	while(std::getline(file, currentCommand)){
+		// Convert command from string to char*
+		int commandLength = currentCommand.length();
+		char* command = new char[commandLength + 1];
+		strcpy(command, currentCommand.c_str());
+
+		// Parse command (using parser from HW1)
+		char* args[MAX_ARG];
+		const char* delimiters = " \t\n";
+		int num_arg = 0;
+		char* cmd = strtok(command, delimiters);
+		if(NULL == cmd){
+			delete[] command;
+			continue;
+		}
+		args[0] = cmd;
+		for(int i = 1; i < MAX_ARG; ++i){
+			args[i] = strtok(NULL, delimiters);
+			if(NULL != args[i]){
+				++num_arg;
+			}
+		}
+
+		// Check current command and direct to relevant function
+		char cmdChar = cmd[0];
+		switch (cmdChar){
+		case 'O':
+			bank.CreateAccount(atoi(args[1]), args[2], atoi(args[3]));
+			// TODO: log, sleep, and a bunch of stuff
+			break;
+		case 'L':
+			// TODO: vip account
+			break;
+		case 'D':
+			// TODO: deposit
+			break;
+		case 'W':
+			// TODO: withdraw
+			break;
+		case 'B':
+			// TODO: check balance
+			break;
+		case 'T':
+			// TODO: transfer money
+			break;
+		default:
+			// TODO: illegal action (not needed)
+			break;
+		}
+		delete[] command;
+	}
 }
--- main.cpp
+++ main.cpp
@@ -5,19 +5,80 @@
  */
 
 #include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <pthread.h>
 
 #include "Bank.h"
-using namespace std;
+#include "Atm.h"
+
+int ValidateInputArguments(int argc, char* argv[]){
+
+	if(argc <= 1){		// Not enough input arguments
+		return -1;
+	}
+
+	int atmsNum = atoi(argv[1]);
+	if(0 >= atmsNum){			// atoi failed, or negative ATMs number
+		return -1;
+	}
+
+	if(atmsNum != (argc - 2)){	// N and number of files doesn't match
+		return -1;
+	}
+
+	return atmsNum;
+}
+
+void* AtmThreadFunction(void* atmInput){
+	Atm atm = *(Atm*) atmInput;
+	atm.Run();
+	pthread_exit(NULL);
+}
 
 int main(int argc, char* argv[]){
-	//Bank bank;
-	//bank.Run();
-	string pass = "abcd";
-	Account account(2345, pass, 0);
-	account.Deposit(pass, 100);
-	account.Withdraw("cdef", 7);
-	account.Withdraw(pass, 111);
-	account.Withdraw(pass, 7);
+
+	int atmsNum = ValidateInputArguments(argc, argv);
+	if(-1 == atmsNum){
+		perror("Illegal program arguments\n");
+		return 0;
+	}
+
+	// Initializations
+	Bank bank;
+	ofstream log;
+	log.open("log.txt");
+	list<Atm> atm_list;
+
+	// Create all Atms
+	for(int i = 0; i < atmsNum; i++){
+		Atm newAtm(bank, i+1, argv[i+2], log);
+		atm_list.push_back(newAtm);
+	}
+
+	// Run all Atms
+	pthread_t* atmThreads = new pthread_t[atmsNum];
+	int i=0;
+	for(list<Atm>::iterator it = atm_list.begin(); it != atm_list.end(); ++it){
+		if(0 != pthread_create(&atmThreads[i], NULL, AtmThreadFunction, (void*) &(*it))){
+			perror("Failed to create thread");
+			delete[] atmThreads;
+			return 0;
+		}
+		++i;
+	}
+
+	// TODO: create printing and commission threads
+
+	// Run all atms
+	for(int i = 0; i < atmsNum; ++i){
+		pthread_join(atmThreads[i], NULL);
+	}
+
+	// TODO: wait for printing and commission thread
+
+	delete[] atmThreads;
+	// Account account(2345, "ct", 0);
 	return 0;
 }
 
